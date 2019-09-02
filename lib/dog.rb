bb54c143bd809742fae35ce7e08a01fe742b831e
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-02 18:50:59 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,7 +52,7 @@ end
 
 def self.find_or_create_by(name:, breed:)
   sql = <<-SQL
-  SELECT * FROM dogs WHERE name = ? breed = ?
+  SELECT * FROM dogs WHERE name = ? AND breed = ?
   SQL
   dog = DB[:conn].execute(sql, name, breed).first
   if dog
