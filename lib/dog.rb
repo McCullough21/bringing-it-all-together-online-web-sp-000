3ea6b6e5423e1bc7d2717383861823c2c8609541
Michael McCullough <michael.mccullough21@gmail.com>
2019-09-02 18:43:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,9 +34,9 @@ def self.create(hash)
 end
 def self.new_from_db(row)
   dog_hash = {
-    id: => row[0]
-    name: => row[1]
-    breed: => row[2]
+    :id => row[0]
+    :name => row[1]
+    :breed => row[2]
   }
   self.new(dog_hash)
 end
